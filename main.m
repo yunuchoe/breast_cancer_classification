3c21ff51e419516657cf7c892424303188e98263
Yunu Choe <yunuchoe10@gmail.com>
2026-04-26 02:38:34 -0700

    Update main.m

--- main.m
+++ main.m
@@ -1,33 +1,34 @@
+% load dataset
 load("D_wdbc.mat")
 
 % get data sets
-Dtr = D_wdbc(:,1:285);
-Dte = D_wdbc(:,286:569);
+Dtr = D_wdbc(:, 1:285);
+Dte = D_wdbc(:, 286:569);
 
 % normalize training data
-Xtr = zeros(30,285);
-m = zeros(1,30);
-v = zeros(1,30);
+Xtr = zeros(30, 285);
+m = zeros(1, 30);
+v = zeros(1, 30);
 for i = 1:30
-    xi = Dtr(i,:);
+    xi = Dtr(i, :);
     m(i) = mean(xi);
     v(i) = sqrt(var(xi));
-    Xtr(i,:) = (xi - m(i))/v(i);
+    Xtr(i, :) = (xi - m(i)) / v(i);
 end
 
 % normalize test data
-Xte = zeros(30,284);
+Xte = zeros(30, 284);
 for i = 1:30
-    xi = Dte(i,:);
-    Xte(i,:) = (xi - m(i))/v(i);
+    xi = Dte(i, :);
+    Xte(i, :) = (xi - m(i)) / v(i);
 end
 
 % assign labels
-ytr = Dtr(31,:);
-yte = Dte(31,:);
+ytr = Dtr(31, :);
+yte = Dte(31,: );
 
 D = [Xtr; ytr];
-w_hat = zeros(31,1);
+w_hat = zeros(31, 1);
 
 % let mu = 0 and k = 10
 mu = 0;
